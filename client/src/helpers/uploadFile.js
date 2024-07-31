94da37aa9c2146c200863eef521cd3580cbcf6f8
pushkarjambhulkar <pushkarjambhulkar200@gmail.com>
2024-07-31 22:29:17 +0530

    done

--- client/src/helpers/uploadFile.js
+++ client/src/helpers/uploadFile.js
@@ -1,6 +1,7 @@
 const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/auto/upload`
 
 const uploadFile = async(file)=>{
+   
     const formData = new FormData()
     formData.append('file',file)
     formData.append("upload_preset","chat-app-file")
