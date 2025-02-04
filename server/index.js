16eb31bc60de341ad43929805e8a7f1f8f232b00
pushkarjambhulkar <pushkarjambhulkar200@gmail.com>
2025-02-04 18:50:39 +0530

    add bodyparser

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const connectDB = require('./config/connectDB')
 const router = require('./routes/index')
 const cookiesParser = require('cookie-parser')
 const { app, server } = require('./socket/index')
+const bodyParser = require('body-parser')
 
 // const app = express()
 app.use(cors({
@@ -14,6 +15,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookiesParser())
+app.use(bodyParser.json())
 
 const PORT = process.env.PORT || 8080
 
